ced692bab80a0fac27980a05b2bd34197b73fe7e
Franpan94 <francescopanico94@libero.it>
2022-07-15 15:39:20 +0200

    creation of scss variables

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        footer
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainFooter',
+}
+</script>
+
+<style lang = 'scss' scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+       header
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader'
+}
+</script>
+
+<style lang = 'scss' scoped>
+
+@import "../styles/variables-header.scss";
+
+</style>
