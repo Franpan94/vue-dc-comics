9f274f607af11855924030afb31387a48f69f3d8
Franpan94 <francescopanico94@libero.it>
2022-07-18 15:10:31 +0200

    creation props

---
+++ src/components/Jumbotroncard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    props: {
+        'thumburl': String,
+        'currentprice': String,
+        'serie': String,
+        'currentype': String,
+    }
+}
+</script>
+
+<style lang = 'scss' scoped>
+</style>
